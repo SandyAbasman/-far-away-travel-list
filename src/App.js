f5769b7c923c83abfd48eba8307edfc5adcf8993
Sandy Abasman <abasifrekesandy@gmail.com>
2024-06-21 10:53:16 +0400

    working on logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "books", quantity: 5, packed: true },
-  { id: 2, description: "clothes", quantity: 6, packed: true },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 2, description: "books", quantity: 5, packed: true },
+//   { id: 2, description: "clothes", quantity: 6, packed: true },
+// ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !Item.packed } : item
+      )
+    );
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <ParkingList />
-      <Stats />
+      <Form onAddItems={handleAddItems} />
+      <ParkingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToogleItem={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -21,12 +42,27 @@ export default function App() {
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
-function Form() {
+
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(7);
+  const [quantity, setQuantity] = useState(1);
+
+  //the handleAddItems takes an item array and the uses the setitems
+  //function to spread the current items and add the new item
+  // this is done in an immutable way
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+    //if no description is entered do nothing
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
@@ -56,34 +92,61 @@ function Form() {
     </form>
   );
 }
-function ParkingList() {
+
+function ParkingList({ items, onDeleteItem, onToogleItem }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {items.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToogleItem={onToogleItem}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToogleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToogleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity}
         {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+      {/*{() => onDeleteItem(item.id)the onclick calls the function only when the event happens*/}
     </li>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list </em>
+      </p>
+    );
+
+  const numItem = items.length;
+  const numpacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numpacked / numItem) * 100);
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "you got everything ready to go "
+          : `💼 You have ${numItem} items on your list, and you already packed
+        ${numpacked} items (${percentage}%)`}
+      </em>
     </footer>
   );
 }
